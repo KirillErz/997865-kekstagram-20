44e665251ceeac9e47a948b077553caafaf6ae02
KirillErz <erzylev@gmail.com>
2020-06-11 10:42:51 +0300

    Отступы

--- js/main.js
+++ js/main.js
@@ -38,42 +38,47 @@ var createMocKekstagram = function (quantityObj) {
 var picturesMoc = createMocKekstagram(25);
 var picture = document.querySelector('#picture');
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < picturesMoc.length; i++) {
+var pictures = document.querySelector('.pictures');
 
+for (var i = 0; i < picturesMoc.length; i++) {
   var element = picture.content.cloneNode(true);
+
   element.querySelector('.picture__img').src = picturesMoc[i].url;
   element.querySelector('.picture__likes').textContent = picturesMoc[i].likes;
   element.querySelector('.picture__comments').textContent = picturesMoc[i].comments.length;
   fragment.append(element);
-}
 
-var pictures = document.querySelector('.pictures');
+}
 pictures.append(fragment);
 
 var bigPicture = document.querySelector('.big-picture');
+
 bigPicture.classList.remove('hidden');
-bigPicture.querySelector('.big-picture__img').querySelector('img').src = picturesMoc[0].url;
+bigPicture.querySelector('.big-picture__img img').src = picturesMoc[0].url;
 bigPicture.querySelector('.likes-count').textContent = picturesMoc[0].likes;
 bigPicture.querySelector('.comments-count').textContent = picturesMoc[0].comments.length;
 document.querySelector('.social__caption').textContent = picturesMoc[0].description;
+
+var commentCount = document.querySelector('.social__comment-count');
+var commentsLoader = document.querySelector('.comments-loader');
+var body = document.querySelector('body');
+
+body.classList.add('modal-open');
+commentCount.classList.add('hidden');
+commentsLoader.classList.add('hidden');
 
 var comments = document.querySelector('.social__comments');
 var commentTemplate = document.querySelector('#comment');
 var commentFragment = document.createDocumentFragment();
 
-var commentCount = document.querySelector('.social__comment-count');
-var commentsLoader = document.querySelector('.comments-loader');
-var body = document.querySelector('body');
-body.classList.add('modal-open');
-commentCount.classList.add('hidden');
-commentsLoader.classList.add('hidden');
-
 for (var i = 0; i < picturesMoc[0].comments.length; i++) {
   var elementComment = commentTemplate.content.cloneNode(true);
+
   elementComment.querySelector('.social__picture').src = picturesMoc[0].comments[i].avatar;
   elementComment.querySelector('.social__picture').alt = picturesMoc[0].comments[i].name;
   elementComment.querySelector('.social__text').textContent = picturesMoc[0].comments[i].message;
   commentFragment.append(elementComment);
+
 }
 comments.append(commentFragment);
 
